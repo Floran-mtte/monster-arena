a6ef275234558f3c57bdd9fe6f2d9e9fc48cc16f
Floran Maitte <floran.maitte@fnacdarty.com>
2019-03-27 15:58:47 +0100

    Design page de login et background splashscreen + connexion google fonctionnelle

--- app/src/main/java/fr/arena/monster/monster_arena/SignInActivity.java
+++ app/src/main/java/fr/arena/monster/monster_arena/SignInActivity.java
@@ -1,14 +1,30 @@
 package fr.arena.monster.monster_arena;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewDebug;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int RC_SIGN_IN = 0;
     TextView sign_up;
+    SignInButton googleSignIn;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +36,23 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
 
         setContentView(R.layout.activity_sign_in);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         sign_up = (TextView) findViewById(R.id.to_inscription);
+        googleSignIn = (SignInButton) findViewById(R.id.sign_in_button);
+        googleSignIn.setPadding(0,0,0,0);
+        googleSignIn.setOnClickListener(this);
+        customizeGooglePlusButton(googleSignIn);
 
         sign_up.setOnClickListener(this);
+    }
+
+    protected void onStart() {
+        super.onStart();
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        Log.i("login","already logged");
+
     }
 
     final public void goToSignUp() {
@@ -36,6 +66,60 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.to_inscription:
                 goToSignUp();
                 break;
+            case  R.id.sign_in_button:
+                signIn();
+                break;
         }
     }
+
+    public void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+         String debug =  Integer.toString(requestCode);
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            Log.i("Login","Success");
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Google signIn error", "signInResult:failed code=" + e.getStatusCode());
+
+        }
+    }
+
+    public static void customizeGooglePlusButton(SignInButton signInButton) {
+        for (int i = 0; i < signInButton.getChildCount(); i++)
+        {
+            View v = signInButton.getChildAt(i);
+
+            String test = v.toString();
+
+            if (v instanceof TextView)
+            {
+                TextView tv = (TextView) v;
+                tv.setText(R.string.sign_in_google);
+                tv.setBackgroundResource(R.color.white);
+                tv.setPadding(5,5,5,5);
+                return;
+            }
+
+        }
+    }
+
 }
