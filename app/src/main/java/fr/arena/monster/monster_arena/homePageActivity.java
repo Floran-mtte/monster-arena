c830b7e0d066aea829348c81ea3497b3afa1a4f3
Floran-mtte <floran.maitte@fnacdarty.com>
2019-06-19 14:05:58 +0200

    SearchParty feature

--- app/src/main/java/fr/arena/monster/monster_arena/homePageActivity.java
+++ app/src/main/java/fr/arena/monster/monster_arena/homePageActivity.java
@@ -1,15 +1,30 @@
 package fr.arena.monster.monster_arena;
 
+import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -38,9 +53,14 @@ public class homePageActivity extends AppCompatActivity implements View.OnClickL
 
     Button editDeck;
     Button playButton;
+    Button shopButton;
+    Button settingButton;
     FirebaseFirestore db;
     String TAG = "HomePageActivity";
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    FrameLayout filter;
+    TextView textView;
+    ProgressBar loader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,10 +82,15 @@ public class homePageActivity extends AppCompatActivity implements View.OnClickL
 
         setContentView(R.layout.activity_home_page);
 
+        shopButton = (Button) findViewById(R.id.shop_button);
+        settingButton = (Button) findViewById(R.id.settings_button);
         editDeck = (Button) findViewById(R.id.edit_deck_button);
         playButton = (Button) findViewById(R.id.play_button);
         playButton.setOnClickListener(this);
         editDeck.setOnClickListener(this);
+        filter = findViewById(R.id.filter_layout);
+        textView = findViewById(R.id.textView2);
+        loader = findViewById(R.id.loader);
 
         // Access a Cloud Firestore instance from your Activity
         db = FirebaseFirestore.getInstance();
@@ -107,126 +132,8 @@ public class homePageActivity extends AppCompatActivity implements View.OnClickL
         switch(v.getId())
         {
             case R.id.play_button:
-                final CollectionReference docRef = db.collection("SearchParty");
-                docRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful())
-                        {
-                            if(task.getResult().isEmpty())
-                            {
-                                Long tsLong = System.currentTimeMillis()/1000;
-                                String ts = tsLong.toString();
-
-                                Map<String, Object> party = new HashMap<>();
-                                party.put("id_user",user.getUid());
-                                party.put("timestamp", ts);
-                                party.put("id_opponent","");
-
-
-                                db.collection("SearchParty")
-                                        .add(party)
-                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                                            @Override
-                                            public void onSuccess(final DocumentReference documentReference) {
-
-                                                final DocumentReference docRef = db.collection("SearchParty").document(documentReference.getId());
-                                                docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
-                                                    @Override
-                                                    public void onEvent(@Nullable DocumentSnapshot snapshot,
-                                                                        @Nullable FirebaseFirestoreException e) {
-                                                        if (e != null) {
-                                                            Log.w(TAG, "Listen failed.", e);
-                                                            return;
-                                                        }
-
-                                                        if (snapshot != null && snapshot.exists()) {
-
-                                                            Log.d(TAG, "Current data: " + snapshot.getData());
-                                                            Map<String, Object> search_party = snapshot.getData();
-                                                            String id_opponent = search_party.get("id_opponent").toString();
-                                                            if(id_opponent != "")
-                                                            {
-                                                                Map<String, Object> party = new HashMap<>();
-                                                                Long tsLong = System.currentTimeMillis()/1000;
-                                                                String ts = tsLong.toString();
-
-                                                                party.put("id", documentReference.getId());
-                                                                party.put("time_game", 0);
-                                                                party.put("number_round", 0);
-                                                                party.put("created_at", ts);
-                                                                party.put("updated_at", ts);
-
-                                                                db.collection("Party").document(documentReference.getId())
-                                                                        .set(party, SetOptions.merge())
-                                                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                                            @Override
-                                                                            public void onSuccess(Void aVoid) {
-                                                                                Log.d("Added doc", "added");
-                                                                            }
-                                                                        })
-                                                                        .addOnFailureListener(new OnFailureListener() {
-                                                                            @Override
-                                                                            public void onFailure(@NonNull Exception e) {
-                                                                                Log.w(TAG, "Error writing document", e);
-                                                                            }
-                                                                        });
-                                                            }
-
-
-                                                        } else {
-                                                            Log.d(TAG, "Current data: null");
-                                                        }
-                                                    }
-                                                });
-
-                                            }
-                                        })
-                                        .addOnFailureListener(new OnFailureListener() {
-                                            @Override
-                                            public void onFailure(@NonNull Exception e) {
-                                                Log.w(TAG, "Error writing document", e);
-                                            }
-                                        });
-
-                            }
-                            else
-                            {
-                                for (final QueryDocumentSnapshot document : task.getResult())
-                                {
-                                    Map<String, Object> party = new HashMap<>();
-                                    //party.put("id_user",search_party.get("id_user").toString());
-                                    //party.put("timestamp", search_party.get("timestamp").toString());
-                                    party.put("id_opponent",user.getUid());
-                                    db.collection("SearchParty").document(document.getId())
-                                            .set(party, SetOptions.merge())
-                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                @Override
-                                                public void onSuccess(Void aVoid) {
-                                                    Log.d("Added doc", document.getId());
-                                                }
-                                            })
-                                            .addOnFailureListener(new OnFailureListener() {
-                                                @Override
-                                                public void onFailure(@NonNull Exception e) {
-                                                    Log.w(TAG, "Error writing document", e);
-                                                }
-                                            });
-                                    Log.d("testing app",document.getId());
-                                }
-                            }
-
-                            for(QueryDocumentSnapshot document : task.getResult())
-                            {
-                                //récupération de la ligne et initialisation
-                            }
-                        }
-                        else
-                        {
-                            Log.d(TAG, "get failed with ", task.getException());
-                        }
-                    }
-                });
+                configView();
+                searchParty();
                 break;
             case R.id.edit_deck_button:
                 //Intent intent = new Intent(this, homePageActivity.class);
@@ -234,8 +141,190 @@ public class homePageActivity extends AppCompatActivity implements View.OnClickL
                 break;
             case R.id.shop_button:
                 break;
-            case R.id.settings_text:
+            case R.id.settings_button:
                 break;
         }
     }
+
+    public void configView()
+    {
+        filter.setVisibility(View.VISIBLE);
+        textView.setVisibility(View.VISIBLE);
+        loader.setVisibility(View.VISIBLE);
+
+        playButton.setEnabled(false);
+        editDeck.setEnabled(false);
+        shopButton.setEnabled(false);
+        settingButton.setEnabled(false);
+    }
+
+    public void searchParty()
+    {
+        final CollectionReference docRef = db.collection("SearchParty");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful())
+                {
+                    //if searchParty is empty we create line
+                    if(task.getResult().isEmpty())
+                    {
+                        Long tsLong = System.currentTimeMillis()/1000;
+                        String ts = tsLong.toString();
+
+                        Map<String, Object> party = new HashMap<>();
+                        party.put("id_user",user.getUid());
+                        party.put("timestamp", ts);
+                        party.put("id_opponent","");
+
+                        db.collection("SearchParty")
+                                .add(party).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(final DocumentReference documentReference)
+                            {
+                                waitForOpponent(documentReference.getId());
+                            }
+                        })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e)
+                                    {
+                                        Log.w(TAG, "Error writing document", e);
+                                    }
+                                });
+
+                    }
+                    else
+                    {
+                        for (final QueryDocumentSnapshot document : task.getResult())
+                        {
+                            joinParty(document.getId());
+                        }
+                    }
+                }
+                else
+                {
+                    Log.d(TAG, "get failed with", task.getException());
+                }
+            }
+        });
+    }
+
+    public void waitForOpponent(String documentId)
+    {
+        final DocumentReference docRef = db.collection("SearchParty").document(documentId);
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                if (e != null)
+                {
+                    Log.w(TAG, "Listen failed.", e);
+                    return;
+                }
+
+                if (snapshot != null && snapshot.exists())
+                {
+
+                    Map<String, Object> search_party = snapshot.getData();
+                    String id_opponent = search_party.get("id_opponent").toString();
+                    if(id_opponent != "")
+                    {
+                        Map<String, Object> party = new HashMap<>();
+                        Long tsLong = System.currentTimeMillis()/1000;
+                        String ts = tsLong.toString();
+
+                        party.put("id", documentId);
+                        party.put("time_game", 0);
+                        party.put("number_round", 0);
+                        party.put("created_at", ts);
+                        party.put("updated_at", ts);
+
+                        db.collection("Party").document(documentId)
+                                .set(party, SetOptions.merge())
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d("Added doc", "added");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error writing document", e);
+                                    }
+                                });
+                    }
+
+
+                }
+                else
+                {
+                    Log.d(TAG, "Current data: null");
+                }
+            }
+        });
+    }
+
+    public void joinParty(String documentId)
+    {
+        Map<String, Object> party = new HashMap<>();
+        party.put("id_opponent",user.getUid());
+        db.collection("SearchParty").document(documentId)
+                .set(party, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        String idParty = documentId;
+
+                        final DocumentReference docRef = db.collection("Party").document(idParty);
+                        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                            @Override
+                            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                                @Nullable FirebaseFirestoreException e) {
+                                if (e != null)
+                                {
+                                    Log.w(TAG, "Listen failed.", e);
+                                    return;
+                                }
+
+                                if (snapshot != null && snapshot.exists())
+                                {
+                                    Log.d(TAG, "Current data: " + snapshot.getData());
+
+                                    db.collection("SearchParty").document(idParty)
+                                            .delete()
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                                                    //go to gameBoardActivity
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Log.w(TAG, "Error deleting document", e);
+                                                }
+                                            });
+
+                                }
+                                else
+                                {
+                                    //RAS
+                                }
+                            }
+                        });
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+        Log.d("testing app",documentId);
+    }
+
 }
