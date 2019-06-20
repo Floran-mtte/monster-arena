ae291402adb7ecaf84d73439bcbd3f3da864c5fd
Floran-mtte <floran.maitte@fnacdarty.com>
2019-06-20 09:30:31 +0200

    GameBoardFront

---
+++ app/src/main/java/fr/arena/monster/monster_arena/gameBoardActivity.java
@@ -0,0 +1,13 @@
+package fr.arena.monster.monster_arena;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gameBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_board);
+    }
+}
